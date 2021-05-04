15c0520ea7d5198102fd0da1b2554909574ae78f
haticeilbegi <57313359+haticeilbegi@users.noreply.github.com>
2021-05-05 02:11:32 +0300

    Add files via upload

---
+++ anti-xss-middleware/Model/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace anti_xss_middleware.Model
+{
+    public class ErrorResponse
+    {
+        public int ErrorCode { get; set; }
+        public string Description { get; set; }
+    }
+}
